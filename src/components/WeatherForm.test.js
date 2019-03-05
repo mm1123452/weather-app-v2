bb158b268891f41f719a5110e5dcccee69e27cc9
Mabel <macmota@hotmail.com>
2019-03-05 14:52:03 -0500

    removed unnecessary spaces

--- src/components/WeatherForm.test.js
+++ src/components/WeatherForm.test.js
@@ -7,36 +7,36 @@ import Button from 'react-bootstrap/Button'
 describe('WeatherForm Component', () => {
 	let props = '1'
 	beforeEach(() => {
-       props = {
-				fourDayForecast: {
-					'1': {
-						id: '1',
-						temp: 42.7,
-						icon: 'wind',
-						forecast:'Windy' ,
-					},
-					'2': {
-						id: '2',
-						temp: 52.7,
-						icon: 'Spring',
-						forecast:'Spring-like' ,
-					},
-					'3': {
-						id: '3',
-						temp: 62.7,
-						icon: 'Sun' ,
-						forecast: 'Sunny',
-					},
-					'4': {
-						id: '4',
-						temp: 72.7,
-						icon: 'clear',
-						forecast: 'clear skys',
-					}		
+        props = {
+			fourDayForecast: {
+				'1': {
+					id: '1',
+					temp: 42.7,
+					icon: 'wind',
+					forecast:'Windy' ,
 				},
-				isFetching: false,
-				error: null
-			}
+				'2': {
+					id: '2',
+					temp: 52.7,
+					icon: 'Spring',
+					forecast:'Spring-like' ,
+				},
+				'3': {
+					id: '3',
+					temp: 62.7,
+					icon: 'Sun' ,
+					forecast: 'Sunny',
+				},
+				'4': {
+					id: '4',
+					temp: 72.7,
+					icon: 'clear',
+					forecast: 'clear skys',
+				}		
+			},
+			isFetching: false,
+			error: null
+		}
 	})
 
 	it ('renders a Form element', () => {
@@ -69,8 +69,5 @@ describe('WeatherForm Component', () => {
 		wrapper.find(Form).simulate('submit', { preventDefault() {} })
 
 		expect(spy).toHaveBeenCalled()
-
 	})
-
-
 })
