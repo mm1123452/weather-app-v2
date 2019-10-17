dc9511806252e412b95db9815b756615aac0c049
Mabel <macmota@hotmail.com>
2019-10-17 18:10:50 -0400

    Updates

---
+++ precache-manifest.55d4ff26bac0805c6a6ecc89f6c2937d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c45a5c97eda224ae280f",
+    "url": "/weather-app-v2/static/js/runtime~main.c45a5c97.js"
+  },
+  {
+    "revision": "446a210fd2eced21e4af",
+    "url": "/weather-app-v2/static/js/main.446a210f.chunk.js"
+  },
+  {
+    "revision": "6f3b72b50a690bd36587",
+    "url": "/weather-app-v2/static/js/2.6f3b72b5.chunk.js"
+  },
+  {
+    "revision": "446a210fd2eced21e4af",
+    "url": "/weather-app-v2/static/css/main.cd0b3c4a.chunk.css"
+  },
+  {
+    "revision": "4353851f57731950c9e88436424e40b7",
+    "url": "/weather-app-v2/index.html"
+  }
+];
